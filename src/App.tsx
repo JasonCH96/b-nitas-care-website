2cb3b63e35e83f805280d866f2970f137c1f306d
Jason Castro <jasoncastroh@gmail.com>
2026-01-14 11:05:24 -0600

    Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename ="/b-nitas-care-website">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter basename="/b-nitas-care-website/">
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
